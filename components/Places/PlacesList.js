99f39d37ad804fe55065d4cf3fa2aa04cf57e31f
fayob <adedejifavour6@gmail.com>
2023-12-20 16:56:04 +0100

    Fix render places issue

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,4 +1,5 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+
 import PlaceItem from './PlaceItem';
 import { Colors } from '../../constants/colors';
 
@@ -6,17 +7,20 @@ function PLacesList({ places }) {
   if (!places || places.length == 0) {
     return (
       <View style={styles.fallbackContainer}>
-        <Text style={styles.fallbackText}>You have not added any place yet</Text>
+        <Text style={styles.fallbackText}>
+          You have not added any place yet
+        </Text>
       </View>
-    )
+    );
   }
 
-  function renderPlaces({ place }) {
-    return <PlaceItem place={place} />;
+  function renderPlaces(place) {
+    return <PlaceItem place={place.item} />;
   }
 
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={renderPlaces}
@@ -27,13 +31,16 @@ function PLacesList({ places }) {
 export default PLacesList;
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 24,
+  },
   fallbackContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   fallbackText: {
     fontSize: 16,
-    color: Colors.primary200
-  }
-})
+    color: Colors.primary200,
+  },
+});
