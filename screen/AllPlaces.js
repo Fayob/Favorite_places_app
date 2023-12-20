cae067feac742d704f0829ad7df35349743f9936
fayob <adedejifavour6@gmail.com>
2023-12-20 16:54:10 +0100

    Update screen pages

--- screen/AllPlaces.js
+++ screen/AllPlaces.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react"
+import { useIsFocused } from "@react-navigation/native"
+
 import PLacesList from "../components/Places/PlacesList"
 
-function AllPlaces() {
-  return <PLacesList />
+function AllPlaces({route}) {
+  const [loadedPlaces, setLoadedPlaces] = useState([])
+  const isFocused = useIsFocused()
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces(currPlaces => [...currPlaces, route.params.place])
+    }
+  }, [isFocused, route])
+
+  return <PLacesList places={loadedPlaces} />
 }
 
 export default AllPlaces
